e351338d21e2d10c9c90a2a45fe8478475f4a8ce
myungjun12 <nenncmj@naver.com>
2024-05-22 17:18:17 +0900

    유효성검사

--- js/23_onkey-onpress.js
+++ js/23_onkey-onpress.js
@@ -2,4 +2,8 @@ function 기능만들기(){
     const 기능 = document.getElementById("영문이름");
     기능.value = 기능.value.toUpperCase();
 }
-
+function 엔터기능(e){
+    if(e.key === 'Enter'){
+        alert("입력한 내용이 전달되었습니다.");
+    }
+}
