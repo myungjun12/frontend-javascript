4a81fc3b7662297bce222bf65e1c3e9caccdff09
myungjun12 <nenncmj@naver.com>
2024-05-23 14:26:11 +0900

    jQuery시작

--- js/18_토글.js
+++ js/18_토글.js
@@ -1,6 +1,6 @@
 function 보이기숨기기(){
     const 박스 = document.getElementById("토글박스");
-    
+   
     // 클래스 목록에 hidden 클래스가 포함되어 있는지 확인
     if(박스.classList.contains("hidden")){
         //만약에 존재한다면 hidden을 지워죠
